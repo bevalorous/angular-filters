2e1875b5ba1ee665b8b6fc4abf36b6fbbafd9187
Vadim Belorussov <belorussov.vadim@gmail.com>
2016-05-11 17:42:19 +0500

    Add scale param

--- src/filters/temperature/temperature.js
+++ src/filters/temperature/temperature.js
@@ -4,7 +4,13 @@ angular.module('bevalorous.angular.filters')
     .filter('temperature', [
         '$filter',
         function ($filter) {
-            return function (number, fractionSize) {
+            return function (number, scale, fractionSize) {
+                if (!scale) {
+                    scale = 'C';
+                }
+                if (!fractionSize) {
+                    fractionSize = 0;
+                }
                 var result = $filter('number')(Math.abs(number), fractionSize),
                     sign = "";
                 if (number > 0) {
@@ -12,7 +18,7 @@ angular.module('bevalorous.angular.filters')
                 } else if (number < 0) {
                     sign = "−";
                 }
-                return sign + result;
+                return sign + result + ' °' + scale;
             };
         }
     ]);
--- test/temperature/temperatureSpec.js
+++ test/temperature/temperatureSpec.js
@@ -1,36 +1,53 @@
 'use strict';
 
 describe('temperature', function () {
-  var temperatureFilter,
-      randomTemperature = Math.random();
+    var temperatureFilter,
+        randomTemperature = Math.random();
 
-  beforeEach(module('bevalorous.angular.filters'));
+    beforeEach(module('bevalorous.angular.filters'));
 
-  beforeEach(inject(function ($filter) {
-    temperatureFilter = $filter('temperature');
-  }));
+    beforeEach(inject(function ($filter) {
+        temperatureFilter = $filter('temperature');
+    }));
 
-  it('should format non-temperature', function () {
-    expect(temperatureFilter("abc", 0)).toEqual("");
-  });
+    describe("format different temperatures", function () {
+        it('should format non-temperature', function () {
+            expect(temperatureFilter("abc")).toEqual(" °C");
+        });
 
-  it('should format zero temperature', function () {
-    expect(temperatureFilter(0, 0)).toEqual("0");
-  });
+        it('should format zero temperature', function () {
+            expect(temperatureFilter(0)).toEqual("0 °C");
+        });
 
-  it('should format integer positive temperature', function () {
-    expect(temperatureFilter(12, 0)).toEqual("+12");
-  });
+        it('should format integer positive temperature', function () {
+            expect(temperatureFilter(12)).toEqual("+12 °C");
+        });
 
-  it('should format integer negative temperature', function () {
-    expect(temperatureFilter(-12, 0)).toEqual("−12");
-  });
+        it('should format integer negative temperature', function () {
+            expect(temperatureFilter(-12)).toEqual("−12 °C");
+        });
 
-  it('should format float positive temperature', function () {
-    expect(temperatureFilter(12.345, 1)).toEqual("+12.3");
-  });
+        it('should format float positive temperature', function () {
+            expect(temperatureFilter(12.345, 'C', 1)).toEqual("+12.3 °C");
+        });
 
-  it('should format float negative temperature', function () {
-    expect(temperatureFilter(-12.345, 1)).toEqual("−12.3");
-  });
+        it('should format float negative temperature', function () {
+            expect(temperatureFilter(-12.345, 'C', 1)).toEqual("−12.3 °C");
+        });
+    });
+
+    describe("format different scales", function () {
+        it('should format zero temperature', function () {
+            expect(temperatureFilter(0, 'F')).toEqual("0 °F");
+        });
+
+        it('should format integer positive temperature', function () {
+            expect(temperatureFilter(12, 'F')).toEqual("+12 °F");
+        });
+
+        it('should format integer negative temperature', function () {
+            expect(temperatureFilter(-12, 'F')).toEqual("−12 °F");
+        });
+    });
+
 });
